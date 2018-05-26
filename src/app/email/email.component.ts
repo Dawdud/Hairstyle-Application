3a50eef4fedbecbc9e49e7f6c0dabfaef7b0b3cc
Daniel Cejrowski <dcpoison@poczta.onet.pl>
2018-05-26 09:45:17 +0200

    rzutem na taśmę działający email component (firebasowy)

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../auth.service';
+import {AngularFirestore} from 'angularfire2/firestore';
 
 @Component({
   selector: 'app-email',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailComponent implements OnInit {
 
-  constructor() { }
+  messageName: String;
+  messageEmail: String;
+  messageText: String;
+
+  messageSent: boolean;
+
+  constructor(private db: AngularFirestore, public authService: AuthService) { }
 
   ngOnInit() {
+    }
+
+  addMessage() {
+    this.db.collection('messages').add({
+      from: this.messageName,
+      email: this.messageEmail,
+      message: this.messageText,
+    })
+      .then(function(docRef) {
+        console.log('Pomyślnie dodano wiadomość o Id = ', docRef.id);
+      });
+    this.messageSent = true;
   }
 
 }
