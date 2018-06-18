342d1ca8ad77946bb8cfd56f5e072a97ba4a7e16
Daniel Cejrowski <dcpoison@poczta.onet.pl>
2018-06-18 20:13:35 +0200

    randomowe 3 opinie, działające :)

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,11 +1,10 @@
 ///<reference path="../../../node_modules/@types/node/index.d.ts"/>
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import {Observable, ObservableInput} from 'rxjs/Observable';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import {AuthService} from '../auth.service';
-import {$} from 'protractor';
-import {AngularFireList} from 'angularfire2/database';
+
 
 
 interface Comment {
@@ -25,9 +24,12 @@ export class CommentsComponent implements OnInit {
 
   commentsCol: AngularFirestoreCollection<Comment>;
   comments: Observable<Comment[]>;
-  quer: Observable<Comment[]>;
-  querCol: AngularFirestoreCollection<Comment>;
 
+  // test zapytania
+  querCol: AngularFirestoreCollection<Comment>;
+  quer: Observable<Comment[]>;
+
+  rand: string;
 
   constructor(private db: AngularFirestore, public authService: AuthService) { }
 
@@ -36,9 +38,15 @@ export class CommentsComponent implements OnInit {
     this.commentsCol = this.db.collection('comments');
     this.comments = this.commentsCol.valueChanges();
 
+    this.rand = pickLetter();
+    console.log(this.rand);
+
     this.querCol = this.db.collection('comments', ref => {
-      return ref.where('stars', '==', 5);
+      return ref.orderBy('name').limit(3).startAt(this.rand);
     });
+
+
+
     this.quer = this.querCol.valueChanges();
 
 
@@ -47,10 +55,18 @@ export class CommentsComponent implements OnInit {
     console.log(this.querCol);
 
     console.log(this.commentsCol.ref.where('stars', '==', 5));
-    console.log("Baza danych:");
-    console.log(this.db);
-    console.log("czy user jest zalogowany?:");
+    console.log('Baza danych:');
+    console.log('db:');
+    console.log('czy user jest zalogowany?:');
     console.log(this.authService.isUserEmailLoggedIn);
+
+    function pickLetter() {
+      let letter = '';
+      let possible = 'ALMRS';
+      letter = possible.charAt(Math.floor(Math.random() * possible.length));
+      return letter;
+    }
+
 
   }
 
