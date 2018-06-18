8f94db6a7bdbc53e0f71267ccb62332be332a9d9
Daniel Cejrowski <dcpoison@poczta.onet.pl>
2018-06-18 18:31:20 +0200

    działające query na komentarzach v 0.1

--- src/app/comments/comments.component.ts
+++ src/app/comments/comments.component.ts
@@ -1,10 +1,11 @@
 ///<reference path="../../../node_modules/@types/node/index.d.ts"/>
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
+import {Observable, ObservableInput} from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import {AuthService} from '../auth.service';
 import {$} from 'protractor';
+import {AngularFireList} from 'angularfire2/database';
 
 
 interface Comment {
@@ -19,10 +20,14 @@ interface Comment {
   templateUrl: './comments.component.html',
   styleUrls: ['./comments.component.scss']
 })
+
 export class CommentsComponent implements OnInit {
 
   commentsCol: AngularFirestoreCollection<Comment>;
   comments: Observable<Comment[]>;
+  quer: Observable<Comment[]>;
+  querCol: AngularFirestoreCollection<Comment>;
+
 
   constructor(private db: AngularFirestore, public authService: AuthService) { }
 
@@ -31,7 +36,20 @@ export class CommentsComponent implements OnInit {
     this.commentsCol = this.db.collection('comments');
     this.comments = this.commentsCol.valueChanges();
 
+    this.querCol = this.db.collection('comments', ref => {
+      return ref.where('stars', '==', 5);
+    });
+    this.quer = this.querCol.valueChanges();
+
+
+
+
+    console.log(this.querCol);
+
+    console.log(this.commentsCol.ref.where('stars', '==', 5));
+    console.log("Baza danych:");
     console.log(this.db);
+    console.log("czy user jest zalogowany?:");
     console.log(this.authService.isUserEmailLoggedIn);
 
   }
